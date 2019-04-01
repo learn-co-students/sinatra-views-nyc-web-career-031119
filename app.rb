e3fcba8b9510a05c27c4d3a1dbe2f0bd67529ea4
Caridad Rivera <ccrive10@gmail.com>
2019-04-01 10:51:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
 
-	get '/' do
-		
+	class App < Sinatra::Base
+
+		get '/' do
+			erb :index
+		end
+
+	  get '/info' do
+	     erb :info
+	  end
 	end
-end
